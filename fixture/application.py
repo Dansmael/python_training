c7690602f01f2b49ad90dac9c9c99f9d140316e1
Daria YAD <dansmael@ya.ru>
2018-05-27 10:39:47 +0700

    Выделен вспомогательный класс для работы с контактами.

--- fixture/application.py
+++ fixture/application.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
+from fixture.contact import ContactHelper
 
 class Application:
 
@@ -9,6 +10,7 @@ class Application:
         self.wd.implicitly_wait(60)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
+        self.contact = ContactHelper(self)
 
 
     def open_home_page(self):
@@ -18,31 +20,3 @@ class Application:
 
     def destroy(self):
         self.wd.quit()
-
-
-    def create_new_contact(self, contact):
-        wd = self.wd
-        wd.find_element_by_link_text("add new").click()
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.first_name)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.last_name)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contact.address)
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(contact.first_phone)
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(contact.second_phone)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contact.first_mail)
-        wd.find_element_by_name("email2").click()
-        wd.find_element_by_name("email2").clear()
-        wd.find_element_by_name("email2").send_keys(contact.second_mail)
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-        self.open_home_page()
--- test/add_new_contact.py
+++ test/add_new_contact.py
@@ -14,9 +14,9 @@ def app(request):
 
 def test_add_new_contact(app):
     app.session.login(username="admin", password="secret")
-    app.create_new_contact(Contact(first_name="first name", last_name="last name", address="address",
-                            first_phone="first phone", second_phone="second phone", first_mail="first mail",
-                            second_mail="second mail"))
+    app.contact.create(Contact(first_name="first name", last_name="last name", address="address",
+                               first_phone="first phone", second_phone="second phone", first_mail="first mail",
+                               second_mail="second mail"))
     app.session.logout()
 
 
