06299b9ea5a220791810eae5098642527a2b7bc2
Daria YAD <dansmael@ya.ru>
2018-05-27 09:24:15 +0700

    Внесены исправления до работающего теста.

--- add_new_contact.py
+++ add_new_contact.py
@@ -7,13 +7,13 @@ from application import Application
 
 @pytest.fixture
 def app(request):
-    fixture = Application
+    fixture = Application()
     request.addfinalizer(fixture.destroy)
     return fixture
 
 
 def test_add_new_contact(app):
-    app.login(admin="admin", password="secret")
+    app.login(username="admin", password="secret")
     app.create_new_contact(Contact(first_name="first name", last_name="last name", address="address",
                             first_phone="first phone", second_phone="second phone", first_mail="first mail",
                             second_mail="second mail"))
