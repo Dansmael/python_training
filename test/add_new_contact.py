0ccc2841854edc137a88bdb26a5c7e57da4e8e7a
Daria YAD <dansmael@ya.ru>
2018-05-27 10:23:20 +0700

    Выделен вспомогательный класс для управления сессиями.

--- test/add_new_contact.py
+++ test/add_new_contact.py
@@ -13,10 +13,10 @@ def app(request):
 
 
 def test_add_new_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_new_contact(Contact(first_name="first name", last_name="last name", address="address",
                             first_phone="first phone", second_phone="second phone", first_mail="first mail",
                             second_mail="second mail"))
-    app.logout()
+    app.session.logout()
 
 
