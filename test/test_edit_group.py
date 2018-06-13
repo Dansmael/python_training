ce29fa8489e1811b43ecc1a25e0558d1416a80b4
Daria YAD <dansmael@ya.ru>
2018-06-13 15:07:22 +0700

    Доработанны тесты и вспомогательные методы для групп на выполнение сортировки списков по заданному параметру перед сравнением.

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -5,9 +5,13 @@ def test_edit_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name="New group"))
     old_groups = app.group.get_group_list()
-    app.group.edit(Group(name="New group - changed", header="header - changed", footer="footer - changed"))
+    group = Group(name="New group - changed", header="header - changed", footer="footer - changed")
+    group.id = old_groups[0].id
+    app.group.edit(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 def test_modify_group_name(app):
     if app.group.count() == 0:
