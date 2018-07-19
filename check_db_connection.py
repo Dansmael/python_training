9d7026433f07db2e4a8f256e7fc841264aa5cabf
Daria YAD <dansmael@ya.ru>
2018-07-19 12:07:19 +0700

    ДЗ 22 ошибка

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,11 +1,12 @@
 import pymysql.cursors
 from fixture.orm import ORMFixture
+from model.group import Group
 
-db = ORMFixture(host="127.0.0.1", database="addressbook", user="root", password="")
+db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 
 
 try:
-    l = db.get_group_list()
+    l = db.get_contacts_in_group(Group(id='161'))
 #    cursor.execute("select * from group_list")
     for item in l:
         print(item)
--- fixture/orm.py
+++ fixture/orm.py
@@ -14,6 +14,7 @@ class ORMFixture:
         name = Optional(str, column='group_name')
         header = Optional(str, column='group_header')
         footer = Optional(str, column='group_footer')
+        contacts = Set(lambda: ORMFixture.ORMContact, table="address_in_groups", column="id", reverse="groups", lazy=True)
 
 
     class ORMContact(db.Entity):
@@ -29,10 +30,11 @@ class ORMFixture:
         mobile_phone = Optional(str, column='mobile')
         work_phone = Optional(str, column='work')
         secondary_phone = Optional(str, column='phone2')
-        deprecated = Optional(datetime, column='deprecated')
+        deprecated = Optional(str, column='deprecated')
+        groups = Set(lambda: ORMFixture.ORMGroup, table="address_in_groups", column="group_id", reverse="contacts", lazy=True)
 
 
-    def _init_(self, host, name, user, password):
+    def __init__(self, host, name, user, password):
         self.db.bind('mysql', host=host, database=name, user=user, password=password, conv=decoders)
         self.db.generate_mapping()
         sql_debug(True)
@@ -43,17 +45,19 @@ class ORMFixture:
             return Group(id=str(group.id), name=group.name, header=group.header, footer=group.footer)
         return list(map(convert, groups))
 
+
     @db_session
     def get_group_list(self):
         return self.convert_groups_to_model(select(g for g in ORMFixture.ORMGroup))
 
+
     def convert_contacts_to_model(self, contacts):
         def convert(contact):
-            return Contact(id=str(contact.id), first_name=contact.firstname, last_name=contact.lastname,
-                           address=str(contact.address),
-                           email=contact.email, email2=contact.email2, email3=contact.email3,
-                           home_phone=contact.home_phone, mobile_phone=contact.mobile_phone,
-                           work_phone=contact.work_phone, secondary_phone=contact.secondary_phone)
+            return Contact(id=str(contact.id), first_name=contact.firstname, last_name=contact.lastname)
+#                           address=str(contact.address),
+#                           email=contact.email, email2=contact.email2, email3=contact.email3,
+#                           home_phone=contact.home_phone, mobile_phone=contact.mobile_phone,
+#                           work_phone=contact.work_phone, secondary_phone=contact.secondary_phone)
 
         return list(map(convert, contacts))
 
@@ -61,3 +65,9 @@ class ORMFixture:
     @db_session
     def get_contact_list(self):
         return self.convert_contacts_to_model(select(c for c in ORMFixture.ORMContact if c.deprecated is None))
+
+
+    @db_session
+    def get_contacts_in_group(self, group):
+        orm_group = list(select(g for g in ORMFixture.ORMGroup if g.id == group.id))[0]
+        return self.convert_contacts_to_model(orm_group.contacts)
