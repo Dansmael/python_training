b0edee4cc8108a2d0091ce3cdde02cc0ae9826f3
Daria YAD <dansmael@ya.ru>
2018-07-25 21:27:28 +0700

    Реализована генерация отчётов о выполнении тестов, используя генератор отчетов Allure

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -10,7 +10,6 @@ def test_add_group(app, db, json_groups, check_ui):
         old_groups = db.get_group_list()
     with pytest.allure.step('When I add a group %s to the list' % group):
         app.group.create(group)
-#    assert len(old_groups) + 1 == app.group.count()
     with pytest.allure.step('Then the group list is equal to the old list with the added group'):
         new_groups = db.get_group_list()
         old_groups.append(group)
